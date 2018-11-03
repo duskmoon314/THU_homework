0ecccb75d5b4a76da8764e437e4acc355d05c498
Campbell <duskmoon314@users.noreply.github.com>
2018-11-03 20:20:08 +0800

    Update exp1-1.cpp

--- C- program design/cpp files/exp1-1.cpp
+++ C- program design/cpp files/exp1-1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-/*
+
 int main() {
 	char c1, c2;
 	cout << "请输入一个值给c1";
 	cin >> c1;
 	c2 = c1 + 5;
 	cout << "打印c2的值" << c2 << endl;
+	
 	return 0;
 }
-*/
