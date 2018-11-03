5b067fedad64bccd76a7c9f589b4f4deeacb3e89
Campbell <duskmoon314@users.noreply.github.com>
2018-11-03 20:23:50 +0800

    Update exp4-1.cpp

--- C- program design/cpp files/exp4-1.cpp
+++ C- program design/cpp files/exp4-1.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 double f1(double x);
 double f2(double x, double y);
-/*
+
 int main() {
 	double x, y;
 	cout << "Please input two numbers:" << endl;
 	cin >> x >> y;
 	cout << "for the first formula: f(x)=\\frac{1}{\\sqrt{2\\pi}}e^{-\\frac{x^{2}}{2}} \n" << f1(x) << endl << "for the second formula: f(x)=\\frac{1}{3}\\sin{(x^{2}+y^{2})}\\cos{(x+y)} \n" << f2(x, y) << endl;
 	return 0;
-}//*/
+}
 
 double f1(double x) {
 	double t;
